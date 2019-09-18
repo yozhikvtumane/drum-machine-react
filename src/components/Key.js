8b9b781588595acb5d10d8211f98bd79de2f9b72
yozhikvtumane <toshamatveeev@gmail.com>
2019-09-18 19:13:08 +0200

    ebotnya s Key and Board has ended, fixed

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,6 +2,13 @@ import React from "react"
 
 class Key extends React.Component {
 	
+	render() {
+		return (
+			<div className="key" data-key={this.props.keyId}>
+				<kbd>{this.props.keyName}</kbd>
+			</div>
+		)
+	}
 }
 
 export default Key
