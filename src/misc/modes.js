79867b3571cdb92eba468a2fd36cfaf8bbb70be2
yozhikvtumane <toshamatveeev@gmail.com>
2019-09-20 13:50:05 +0200

    adding samples modules and mode file to preload samples

---
+++ src/misc/modes.js
@@ -0,0 +1,6 @@
+import * as drumSamples from '../static/samples/drums/drumSamples'
+import * as synthSamples from '../static/samples/synths/synthSamples'
+
+const modes = {
+	
+}
---
+++ src/static/samples/synths/synthSamples.js
@@ -0,0 +1,18 @@
+import sampleArp_A1 from './Arp B A1.wav'
+import sampleArp_B1 from './Arp B B1.wav'
+import sampleArp_C1 from './Arp B C1.wav'
+import sampleArp_D1 from './Arp B D1.wav'
+import sampleArp_E1 from './Arp B E1.wav'
+import sampleArp_F1 from './Arp B F1.wav'
+import sampleArp_G1 from './Arp B G1.wav'
+
+
+export {
+	sampleArp_A1,
+	sampleArp_B1,
+	sampleArp_C1,
+	sampleArp_D1,
+	sampleArp_E1,
+	sampleArp_F1,
+	sampleArp_G1
+}
