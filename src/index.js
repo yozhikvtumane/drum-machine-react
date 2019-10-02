91a290e265805a5dbac8aa50ec464c5821250a6e
yozhikvtumane <toshamatveeev@gmail.com>
2019-10-02 15:58:20 +0200

    Added media queries

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import DrumMachine from '../src/containers/DrumMachine';
 import 'normalize.css'
 import './static/styles/index.css'
+import './static/styles/responsive.css'
 import './static/img/switcherUp.svg'
 import './static/img/switcherDown.svg'
 
